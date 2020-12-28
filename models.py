8fb6dbeaf57385f349a754d83a03a5d437497275
Benjamin Ye <benjamin.ye@me.com>
2020-12-28 17:02:47 -0500

    level 1

--- models.py
+++ models.py
@@ -5,50 +5,51 @@ import utils
 
 
 class AirbnbThread:
-	def __init__(self, thread):
-		self.thread = thread
+    def __init__(self, thread):
+        self.thread = thread
 
-	def guest_id(self):
-		return self.thread['id']
+    def guest_id(self):
+        return self.thread['id']
 
-	def updated_at(self):
-		return utils.parse_timestr(self.thread['last_message_sent_at'])
+    def updated_at(self):
+        return utils.parse_timestr(self.thread['last_message_sent_at'])
 
-	def host_id(self):
-		return next((r['user_ids'][0] for r in self.thread['attachment']['roles']
-					 if r['role'] == 'owner'))     
+    def host_id(self):
+        return next((r['user_ids'][0] for r in self.thread['attachment']['roles']
+                     if r['role'] == 'owner'))
 
-	def messages(self):
-		return [AirbnbMessage(m) for m in self.thread['messages']]
+    def messages(self):
+        return [AirbnbMessage(m) for m in self.thread['messages']]
 
-	def guest_name(self):
-		return next((u['first_name'] for u in self.thread['users'] 
-			         if u['id'] != self.host_id()))
+    def guest_name(self):
+        return next((u['first_name'] for u in self.thread['users']
+                     if u['id'] != self.host_id()))
+
 
 class AirbnbMessage:
-	def __init__(self, message):
-		self.msg = message
+    def __init__(self, message):
+        self.msg = message
 
-	def message(self):
-		return self.msg['message']
+    def message(self):
+        return self.msg['message']
 
-	def sent(self):
-		return utils.parse_timestr(self.msg['created_at'])
+    def sent(self):
+        return utils.parse_timestr(self.msg['created_at'])
 
-	def user_id(self):
-		return self.msg['user_id']
+    def user_id(self):
+        return self.msg['user_id']
 
 
 class MessageModel(BaseModel):
-	guest_id: str
-	sent: int # milliseconds timestamp
-	message: str
-	user: Literal['guest', 'owner']
-	channel: Literal['airbnb','SMS','email','whatsapp']
+    guest_id: str
+    sent: int  # milliseconds timestamp
+    message: str
+    user: Literal['guest', 'owner']
+    channel: Literal['airbnb', 'SMS', 'email', 'whatsapp']
 
 
 class GuestModel(BaseModel):
-	guest_id: str
-	updated_at: int # milliseconds timestamp
-	total_msgs: int
-	name: str
+    guest_id: str
+    updated_at: int  # milliseconds timestamp
+    total_msgs: int
+    name: str
--- sync.py
+++ sync.py
@@ -1,5 +1,8 @@
+from typing import List
+import pprint
 import json
 import logging
+
 from models import MessageModel, GuestModel, AirbnbThread
 
 logger = logging.getLogger()
@@ -7,60 +10,85 @@ logger.setLevel(logging.INFO)
 
 
 class AirbnbClient:
-	"""retrieves airbnb threads, do not modify""" 
-	def __init__(self):  
-		pass
-		
-	def get_messages(self, step=1): 
-		assert step in [1,2]
-		with open(f'threads_{step}.json') as file:
-		  return [AirbnbThread(thread) for thread in json.load(file)]
+    """retrieves airbnb threads, do not modify"""
 
+    def __init__(self):
+        pass
+
+    def get_messages(self, step=1) -> List[AirbnbThread]:
+        assert step in [1, 2]
+        with open(f"threads_{step}.json") as file:
+            return [AirbnbThread(thread) for thread in json.load(file)]
 
 
 class SyncAirbnb:
-	"""syncs airbnb threads with enso database"""
-	def __init__(self, client):
-		self.messages = {}
-		self.guests = {}
-		self.client = client
+    """syncs airbnb threads with enso database"""
 
-	def __call__(self, step):
-		airbnb_threads = self.client.get_messages(step)
-		for thread in airbnb_threads:
-			self._create_guest(thread)
-			for msg in thread.messages():
-				self._create_message(thread.guest_id(), 
-									 thread.host_id(), msg)
+    def __init__(self, client):
+        self.messages = {}
+        self.guests = {}
+        self.client = client
+
+    def __call__(self, step):
+        airbnb_threads = self.client.get_messages(step)
+        for thread in airbnb_threads:
+            self._update_guest(thread)
+            for msg in thread.messages():
+                self._update_message(thread.guest_id(), thread.host_id(), msg)
+
+    def _create_message(self, guest_id, host_id, message):
+        user = "owner" if message.user_id() == host_id else "guest"
+        new_msg = MessageModel(
+            guest_id=guest_id,
+            user=user,
+            message=message.message(),
+            channel="airbnb",
+            sent=message.sent(),
+        )
+        self.messages[guest_id].append(new_msg)
+
+    def _create_guest(self, thread):
+        guest_id = thread.guest_id()
+        new_guest = GuestModel(
+            guest_id=guest_id,
+            updated_at=thread.updated_at(),
+            name=thread.guest_name(),
+            total_msgs=len(thread.messages()),
+        )
+        self.messages[guest_id] = []
+        self.guests[guest_id] = new_guest
+
+    def _update_guest(self, thread):
+        guest_id = thread.guest_id()
+
+        if guest_id not in self.guests:
+            self._create_guest(thread)
+            return
+
+        guest = self.guests[guest_id]
+        guest.updated_at = thread.updated_at()
+        guest.total_msgs = len(thread.messages())
+
+    def _update_message(self, guest_id, host_id, message):
+        messages = self.messages[guest_id]
+
+        if not messages:
+            self._create_message(guest_id, host_id, message)
+            return
+
+        message_ts = [(msg.sent, msg.message) for msg in messages]
+
+        if (message.sent(), message.message()) not in message_ts:
+            self._create_message(guest_id, host_id, message)
+            messages.sort(key=lambda x: (x.sent, x.message), reverse=True)
 
 
-	def _create_message(self, guest_id, host_id, message):
-		user = 'owner' if message.user_id() == host_id else 'guest'
-		new_msg = MessageModel(
-			guest_id=guest_id,
-			user=user,
-			message=message.message(),
-			channel='airbnb',
-			sent=message.sent()
-		)
-		self.messages[guest_id].append(new_msg)
+sync_1 = SyncAirbnb(AirbnbClient())
+sync_1(1)
+sync_1(2)
 
-	def _create_guest(self, thread):
-		guest_id = thread.guest_id()
-		new_guest = GuestModel(
-			guest_id=guest_id,
-			updated_at=thread.updated_at(),
-			name=thread.guest_name(),
-			total_msgs=len(thread.messages())
-		)
-		self.messages[guest_id] = []
-		self.guests[guest_id] = new_guest
+sync_2 = SyncAirbnb(AirbnbClient())
+sync_2(2)
 
-	def _update_guest(thread):
-		# TODO Implement
-		pass
-
-
-sync = SyncAirbnb(AirbnbClient())
-sync(1)
-print(sync.guests, sync.messages)
+assert sync_1.guests == sync_2.guests
+assert sync_1.messages == sync_2.messages
